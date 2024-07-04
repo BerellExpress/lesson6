5290b478e4ee362fd5b567acecbb10e43769ee0f
BerellExpress <yury1997@mail.ru>
2024-07-04 22:01:58 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+my_dict = {'Yury': 8996482, 'Kris': 8961688, 'Max': 8999777}
+print(my_dict)
+print(my_dict['Yury'])
+print(my_dict.get('Anton', 'Не найдено'))
+my_dict.update({'Sam': 4254569452,
+                'Kamila': 41556787459})
+print(my_dict.pop('Yury'))
+print(my_dict)
+
+my_set = {1, 2, 3, 'Yury', False, 2, 'Yury'}
+print(my_set)
+my_set.add(list[6, 7])
+my_set.add(5)
+my_set.discard(False)
+print(my_set)
